f72d3bb4712051c086434b9a3904d8c7f0345741
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-01-31 21:26:53 -0300

    wip: apply ocr by video

--- services/plate_recognition/main.py
+++ services/plate_recognition/main.py
@@ -1,6 +1,7 @@
 import yolov5
 import cv2
 import easyocr
+import os
 
 class PlateRecognition:
     def __init__(self, model_path="keremberke/yolov5m-license-plate"):
@@ -15,9 +16,19 @@ class PlateRecognition:
         self.reader = easyocr.Reader(['pt'])
 
     def process_frame(self, frame):
-        results = self.model(frame, size=640)
+        results = self.model(frame, size=640)        
         
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        for detection in results.pred[0]:
+            x1, y1, x2, y2, conf, cls = detection.tolist()
+            print(f"This is conf level: {conf}")
+
+        results.show()
+
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()
+        
+        return frame
+        """frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         for detection in results.pred[0]:
             x1, y1, x2, y2, conf, cls = detection.tolist()
@@ -36,11 +47,35 @@ class PlateRecognition:
         cv2.imwrite('results/plate_recognized.jpg', frame)
 
         return "Process concluded"
+        """
     
+    @staticmethod
+    def process_image_in_folder(folder_path, output_folder):
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        plate_recognition = PlateRecognition()
+
+        for filename in os.listdir(folder_path):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+                image_path = os.path.join(folder_path, filename)
+                frame = cv2.imread(image_path)
+
+                if frame is None:
+                    print(f"Erro: Não foi possível carregar a imagem '{filename}'. Verifique o caminho.")
+                    continue
+
+                processed_frame = plate_recognition.process_frame(frame)
+                
+                output_path = os.path.join(output_folder, filename)
+                cv2.imwrite(output_path, processed_frame)
+
+                print(f"Image processed: {output_path}")
+
+
 if __name__ == "__main__":
-    frame = cv2.imread("records/frame3.jpg")
+    input_folder = "records"
+    output_folder = "results"
+
     plate_recognition = PlateRecognition()
-
-    predictions = plate_recognition.process_frame(frame)
-
-    print(predictions)
+    plate_recognition.process_image_in_folder(input_folder, output_folder)
---
+++ services/plate_recognition/plate_ocr.py
@@ -0,0 +1,2 @@
+def apply_ocr_on_frame_plate() -> str:
+    return "Done ."
