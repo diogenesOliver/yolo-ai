41593bb1590649d60c6a5c0bba6b21ff09b9ab37
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-02-05 21:00:57 -0300

    feat: applying OCR in all images of folder

--- services/plate_recognition/plate_ocr.py
+++ services/plate_recognition/plate_ocr.py
@@ -1,39 +1,36 @@
-import yolov5
 import cv2
 import easyocr
+import os
+import json
+
 
 class PlateRecognitionOCR:
-    def __init__(self, model_path="keremberke/yolov5m-license-plate"):
-        self.model = yolov5.load(model_path) 
-
-        self.model.conf = 0.60  
-        self.model.iou = 0.45  
-        self.model.agnostic = False  
-        self.model.multi_label = False  
-        self.model.max_det = 1000
-
+    def __init__(self):
         self.reader = easyocr.Reader(['pt']) 
 
-    def apply_ocr_on_frame_plate(self, frame):
-        results = self.model(frame)
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    def apply_ocr_on_frame_plate(self, folder_path="records"):
+        for file in os.listdir(folder_path):
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+                frame_path = os.path.join(folder_path, file)
+                
+                frame = cv2.imread(frame_path)
+                result = self.reader.readtext(frame)
 
-        for detection in results.pred[0]:
-            x1, y1, x2, y2, conf, cls = detection.tolist()
+                if frame is None:
+                    print(f"Erro: Não foi possível carregar a imagem '{file}'. Verifique o caminho.")
+                    continue
 
-            if conf >= 0.60:
-                plate_region = frame_rgb[int(y1):int(y2), int(x1):int(x2)]
+                for bbox, text, conf in result:
+                    plate_recognition_data = dict()
 
-                ocr_results = self.reader.readtext(plate_region)
-                plate_text = " ".join([result[1] for result in ocr_results])  
+                    plate_recognition_data['text'] = text
+                    plate_recognition_data['confiability'] = conf
 
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)  
-                cv2.putText(frame, plate_text, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    plate_recognition_json = json.dumps(plate_recognition_data, indent=4)
 
-        cv2.imwrite('results/frame5.jpg', frame)
+                    print(plate_recognition_json)
 
-        return "Process concluded"
+        return "Done ."
     
 
-frame = cv2.imread("records/frame5.jpg")
-PlateRecognitionOCR().apply_ocr_on_frame_plate(frame)
+PlateRecognitionOCR().apply_ocr_on_frame_plate()
