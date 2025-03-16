6047ff9400dc0ff4be32ece34cc6c7be48f4072a
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-03-16 19:31:18 -0300

    fix: fixing bug by render video

--- services/sleep-detection/main.py
+++ services/sleep-detection/main.py
@@ -1,6 +1,10 @@
 import cv2 
 import mediapipe as mp
 from ultralytics import YOLO
+import sys
+import os
+
+os.environ["QT_QPA_PLATFORM"] = "xcb"
 
 class SleedDetection:
     def __init__(self):
@@ -8,27 +12,27 @@ class SleedDetection:
         pass
 
     def sleep_detection(self):
-        """cap = cv2.VideoCapture("records/sleep_test.mp4")
+        cap = cv2.VideoCapture("records/sleep_test.mp4")
 
         while cap.isOpened():
             ret, frame = cap.read()
 
             if not ret:
                 break
-            
+
             frame_resized = cv2.resize(frame, (640, 360))
             cv2.imshow("Sleep Detection", frame_resized)
 
             if cv2.waitKey(25) & 0xFF == ord("q"):
                 break
 
-            return "Done .""
-        """
+        cap.release()
+        cv2.destroyAllWindows()
+        return "Done ." 
 
-        results = self.model.predict(source="records/sleep_test.mp4", save=True, conf=0.4)
-        results.show()
-
-        return "Done ."
-
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "-r":
+        print("Running sleep detection")
+        SleedDetection().sleep_detection()
 
 SleedDetection().sleep_detection()
