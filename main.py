056bc2f9b456985c25080ebc0532e0bc4b9c8e50
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-04-08 18:44:41 -0300

    wip: setting argumentos to running code

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import json
+import argparse
+import logging as logs
+
+#from utils.yolo_model_handler import YoloModelHandler
 
 with open('/home/diogenes/Documentos/www/yolo-ai/config.json') as f:
     config = json.load(f)
 
-if config['process']['service'] == 'sleepDetector':
-    from services.sleep_detection.main import SleedDetection
-    SleedDetection().sleep_detection()
-else:
-    print("Invalid service .")
+def main():
+    parser = argparse.ArgumentParser(description='Video management with execution parameters')
 
-if __name__ == "__main__":
-    print("running main.py")
-    pass
+    parser.add_argument('-b','--background', action='store_true', help='Running video in background')
+    parser.add_argument('-r', '--realtime', action='store_true', help='Running video in real time')
+
+    args = parser.parse_args()
+
+    if args.realtime:
+        print('[INFO] - Running in real time')
+    
+    if args.background:
+        print('[INFO] - Running in background')
+
+if __name__ == '__main__':
+
+    main()
