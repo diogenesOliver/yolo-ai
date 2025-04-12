607c6f39095c96cdffe9dda69656db17154adb0b
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-04-11 22:58:42 -0300

    wip: creating main file to process video

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import json
 import argparse
+import os
 
 from utils.yolo_model_handler import YOLOModelHandler
 from video_capture.video_capture_async import VideoProcess
 
-with open('/home/diogenes/Documentos/www/yolo-ai/config.json') as f:
+json_file = "config.json"
+
+with open(json_file, 'r') as f:
     config = json.load(f)
 
 def main():
@@ -16,11 +19,26 @@ def main():
     args = parser.parse_args()
 
     if args.realtime:
-        print('[INFO] - Running in real time')
-    
+        records_path = './records'
+        video_id = config['process']['service'][0]['video_id']
+
+        if os.path.exists(records_path):
+            for file in os.listdir(records_path):
+                pwd_file = os.path.join(records_path, file)
+
+                if os.path.isfile(pwd_file):
+                    file = int(os.path.splitext(file)[0])
+                    
+                    if file == video_id:
+                        # Apply logic to process the video
+
+                        return
+                    else:
+                        print("[ERROR] - No video file found")
+                        return
+
     if args.background:
         print('[INFO] - Running in background')
 
 if __name__ == '__main__':
-
     main()
