5a66fca21f1ab42707d6536c6d8ac3e38272e5ac
diogenesOliver <diogenes.riboliveira@gmail.com>
2025-06-06 14:15:41 -0300

    fixing: rederizing head YOLO detection

--- services/sleep_detection/main.py
+++ services/sleep_detection/main.py
@@ -1,7 +1,20 @@
+import cv2
+
 class SleedDetection:
     def __init__(self):
         pass
 
     def process_frame(self, frame, model):
-        results = model(frame)
+        results = model(frame)[0]
+
+        for box in results.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = float(box.conf[0])
+            cls_id = int(box.cls[0])
+            label = model.names[cls_id]
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
         return frame
